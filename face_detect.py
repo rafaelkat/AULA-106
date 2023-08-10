bfdc2d3b862424135b6388fcb18f926ebd689833
rafaelkat <97891367+rafaelkat@users.noreply.github.com>
2023-08-10 14:55:23 -0300

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,28 @@
+
+import cv2
+
+img = cv2.imread("Outras-pessoas.jpg")
+
+gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+faces = face_cascade.detectMultiScale(gray,1.2, 6)
+# faces = face_cascade.detectMultiScale(gray)
+
+print(len(faces))
+
+for (x,y,w,h) in faces:
+       #cv2.rectangle(imagem, ponto_inicial, ponto_final, cor, espessuraBorda)
+
+       cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)  
+
+       # Corte a imagem para salvar a imagem do rosto.
+       roi_color = img[y:y+h, x:x+w]
+       cv2.imwrite("face.jpg",roi_color)
+              
+cv2.imshow('img',img)
+cv2.waitKey(0)
+
+
+
